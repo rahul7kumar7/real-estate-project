0853f32ba0951cb44964d1ab684804874ae28f8d
Rahul Kumar <rahul.k.mak@gmail.com>
2024-12-23 23:12:51 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
+import listingRoute from "./routes/listing.route.js";
 
 const app = express();
 app.use(express.json());
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRoute);
 app.use((err, req, res, next)=> {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";

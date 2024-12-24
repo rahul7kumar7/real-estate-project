3ff2a890322cd7be03025f6cfd9122cccf331aed
Rahul Kumar <rahul.k.mak@gmail.com>
2024-12-24 22:19:31 +0530

    upload listing images functionality added

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import  {useState} from 'react';
 export default function CreateListing() {
+    const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+    });
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const [uploading, setUploading] = useState(false);
+    console.log(formData);
+    const handleImageSubmit = (e) => {
+        if(files.length > 0 && files.length + formData.imageUrls.length < 7) {
+            // TODO: File size limit
+            setUploading(true);
+            setImageUploadError(false);
+            const promises = []; // cause we are going ot upload more than 1 image
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]));
+            }
+            Promise.all(promises).then(urls => {
+                setFormData({...formData, imageUrls: formData.imageUrls.concat(urls)});
+            setImageUploadError(false);
+            setUploading(false);
+
+            }).catch((err) => {
+                setImageUploadError(err);
+                setUploading(false);
+            })
+        } else {
+            setImageUploadError('You can only upload 6 images per listing.');
+            setUploading(false);
+        }
+    };
+    const storeImage = async (file) => {
+            try {
+                // TODO: USE XHR
+                const data = new FormData();
+                data.append('file', file);
+                data.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+                data.append('cloud_name', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+                const res = await fetch(import.meta.env.VITE_CLOUDINARY_UPLOAD_ENDPOINT, {
+                    method: 'POST',
+                    body: data,
+                });
+                if (!res.ok) {
+                    console.log('Failed to upload image', res.statusText);
+                }
+                const uploadedImageUrl = await res.json();
+                return uploadedImageUrl.url;
+            } catch (error){
+                console.log(error.message);
+            }
+
+        };
+
+    const handleRemoveImage = (index) => {
+        setFormData({...formData, imageUrls: formData.imageUrls.filter((_, i)=> i!== index)});
+    }
+
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>
@@ -110,9 +166,18 @@ export default function CreateListing() {
                         <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
                     </p>
                     <div className="flex gap-4">
-                        <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
-                        <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                        <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple onChange={(e)=> setFiles(e.target.files)} />
+                        <button disabled={uploading} type="button" onClick = {handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>{uploading? 'uploading...' : 'Upload'}</button>
                     </div>
+                    <p className={'text-red-700 text-sm'} > {imageUploadError && imageUploadError} </p>
+                    {
+                        formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+                        <div key={url} className="flex justify-between p-3 border items-center" >
+                            <img src={url} alt="listing image" className='w-20 h-20 object-cover rounded-lg'/>
+                            <button type="button" onClick={()=> handleRemoveImage(index)} className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75">Delete</button>
+                        </div>
+                        ))
+                    }
                     <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
                 </div>
             </form>
